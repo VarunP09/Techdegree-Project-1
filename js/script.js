6685bfb17898e691107e4b5e1926b8ff74ad8894
VarunP09 <9varunpappu9@gmail.com>
2023-11-25 11:23:17 -0500

    Added quote objects and attributes

--- js/script.js
+++ js/script.js
@@ -11,6 +11,38 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+//Creating 5 quote objects within an array
+const quotes = [
+  {
+    quote: "Do. Or do not. There is no try.",
+    source: "Yoda",
+    citation: "Star Wars: Episode V - The Empire Strikes Back",
+    year: 1980
+  },
+  {
+    quote: "I fear no one, but respect everyone.",
+    source: "Roger Federer",
+    //Could not find year
+  },
+  {
+    quote: "I'm gonna make him an offer he can't refuse.",
+    source: "Don Corleone",
+    citation: "The Godfather",
+    year: 1972
+  },
+  {
+    quote: "May the Force be with you.",
+    source: "Obi-Wan Kenobi",
+    citation: "Star Wars"
+    //Didn't cite specific movie or year because it is said multiple times
+  },
+  {
+    quote: "You don't have time to think up there. If you think, you're dead.",
+    source: "Pete Mitchell (AKA Maverick)",
+    citation: "Top Gun",
+    year: 1986
+  }
+];
 
 
 /***
