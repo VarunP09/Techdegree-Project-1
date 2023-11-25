c2e88987f03027589f207f92d2ff404fc36b35d3
VarunP09 <9varunpappu9@gmail.com>
2023-11-25 12:54:48 -0500

    Final Version

--- js/script.js
+++ js/script.js
@@ -12,18 +12,19 @@ project 1 - A Random Quote Generator
 ***/
 
 //Creating 5 quote objects within an array
-
 const quotes = [
   {
     quote: "Do. Or do not. There is no try.",
     source: "Yoda",
     citation: "Star Wars: Episode V - The Empire Strikes Back",
     year: 1980
+    
   },
   {
     quote: "I fear no one, but respect everyone.",
     source: "Roger Federer",
     //Could not find year
+    tag: "inspirational"
   },
   {
     quote: "I'm gonna make him an offer he can't refuse.",
@@ -45,6 +46,8 @@ const quotes = [
   }
 ];
 
+//timer regularly refreshes random quote every 5 seconds (extra credit)
+setInterval(printQuote, 5000);
 
 
 
@@ -52,6 +55,8 @@ const quotes = [
 /***
  * `getRandomQuote` function
 ***/
+
+//Returns randomly selected quote object from quotes array
 function getRandomQuote(arr){
   const index = Math.round(Math.random()*(arr.length-1));
   return arr[index];
@@ -61,22 +66,32 @@ function getRandomQuote(arr){
 /***
  * `printQuote` function
 ***/
+
+
 function printQuote(){
+  //stores random quote object
   const quoteRandom = getRandomQuote(quotes);
 
+  //starts building info to add to page
   let html = `
   <p class="quote">${quoteRandom.quote}</p>
-  <p class="source">${quoteRandom.source}</p>`;
+  <p class="source">${quoteRandom.source}`;
 
+  //checks if we have citation and/or year and adds to html if true
   if(typeof quoteRandom.citation !== 'undefined'){
     html += `<span class="citation">${quoteRandom.citation}</span>`;
   }
   if(typeof quoteRandom.year !== 'undefined'){
     html += `<span class="year">${quoteRandom.year}</span>`;
   }
+  //checks for tag attribute
+  if(typeof quoteRandom.tag !== 'undefined'){
+    html += `, <span>${quoteRandom.tag}</span>`;
+  }
+  html+=`</p>`;
+  //adds info to webpage
   document.getElementById('quote-box').innerHTML = html; 
 }
-
 
 /***
  * click event listener for the print quote button
@@ -84,3 +99,7 @@ function printQuote(){
 ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
+
+
+
